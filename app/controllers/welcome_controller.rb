e0df12316e8c6538f9b3e1342fe46d0a62481c16
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-04-07 23:49:41 -0700

    Creates route that renders generated ipsum   * creates AJAX request to get_ipsum route   * AJAX currently disabled; enabling in progress

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,20 @@
 class WelcomeController < ApplicationController
+  # before_filter?
   def index
+   @ipsum = Ipsum.create_ipsum
   end
 
-  def new
+  def create
+  end
+
+  def get_ipsum
     @ipsum = Ipsum.create_ipsum
     respond_to do |format|
+      format.html
       format.js
     end
+    # if @ipsum
+    #   render :partial => 'ipsum'
+    # end
   end
 end
