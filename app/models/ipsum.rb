292f1149b0f9130b2f9308dd8343c92a356839c1
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-01 17:41:28 -0700

    ipsum model - separates creating ipsum into 2 steps (sort, create)

--- app/models/ipsum.rb
+++ app/models/ipsum.rb
@@ -8,11 +8,25 @@ class Ipsum < ActiveRecord::Base
   #   end
   # end
 
-  def self.create_ipsum #have words loaded into array beforehand?
-    @sweet_words = Array.new
+  def self.sort_by_most_used
+    master_words_arr = Array.new
     CSV.foreach("./lib/sweet_words.csv") do |row|
-      @sweet_words << row
+      master_words_arr << row
     end
-    @final_ipsum = @sweet_words.shuffle!.join(" ").capitalize + "..."
+    master_words_arr.group_by do |e|
+      e
+    end.values.join(" ").split(" ").group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first)
   end
+
+  def self.create_ipsum
+    self.sort_by_most_used.take(300).shuffle!.join(" ").capitalize + "..."
+  end
+
+
+  #   @sweet_words = Array.new
+  #   CSV.foreach("./lib/sweet_words.csv") do |row|
+  #     @sweet_words << row
+  #   end
+  #   @final_ipsum = @sweet_words.shuffle!.join(" ").capitalize + "..."
+  # end
 end
