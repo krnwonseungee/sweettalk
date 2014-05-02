daeeb608bb98fc7df53ee34366b2ffc248821205
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-01 19:34:47 -0700

    Adds method for capitalizing after period.   * wip: capitalizing letter i's

--- app/models/ipsum.rb
+++ app/models/ipsum.rb
@@ -1,22 +1,28 @@
 require 'csv'
 
 class Ipsum < ActiveRecord::Base
-  # def initialize
-  #   @sweet_words = Array.new
-  #   CSV.foreach("./lib/sweet_words.csv") do |row|
-  #     @sweet_words << row
-  #   end
-  # end
 
   def self.sort_by_most_used
     master_words_arr = Array.new
     CSV.foreach("./lib/sweet_words.csv") do |row|
       master_words_arr << row
     end
-    master_words_arr.group_by do |e|
+    unspellchecked = master_words_arr.group_by do |e|
       e
-    end.values.join(" ").split(" ").group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first)
+    end.values.join(" ").split(" ").group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first).take(300)#.join(" ")
+    # unspellchecked.gsub!( /[(i')(i)]/, 'i' => 'I', %q(i') => %q(I') ).split(" ")
   end
+
+  def cap_after_period(arr)
+    uncapped = self.sort_by_most_used
+    uncapped.map! do |word|
+      word.capitalize! if cap_after_period[word.index - 1][-1] == "."
+    end
+  end
+
+  def cap_letter_i
+
+
 
   def self.create_ipsum
     self.sort_by_most_used.take(300).shuffle!.join(" ").capitalize + "..."
