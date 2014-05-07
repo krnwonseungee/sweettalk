25a26b4abb4ad906da52de1ee85f1fb71580fe9f
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-07 01:55:59 -0700

    Correctly creates separate view and controller   * renamed former renderIpsum to createIpsumAjaxRequest

--- app/assets/javascripts/ipsum.js
+++ app/assets/javascripts/ipsum.js
@@ -9,20 +9,25 @@ Controller.prototype = {
   bindEvents: function(){
     $("#ipsumButton").on("click", function(e){
       e.preventDefault();
-      controller.renderIpsum();
+      controller.createIpsumAjaxRequest();
     })
   },
 
-  renderIpsum: function(){
+  createIpsumAjaxRequest: function(){
     $.ajax({
       type: "get",
       url: "/get_ipsum",
     }).done(function(data){
-      $("#ipsumContainer").html(data);
+      view.renderIpsum(data);
     })
   }
 }
 
+View.prototype = {
+  renderIpsum: function(data){
+    $("#ipsumContainer").html(data);
+  }
+}
 
 controller = new Controller();
 view = new View();
