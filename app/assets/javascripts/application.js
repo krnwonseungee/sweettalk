7ff31779cfbe5408d7fd2211a920e3c9f0a68753
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-07 02:31:18 -0700

    Renames js file to application.js to avoid confusion with model file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,34 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+$(document).ready(function(){
+  controller.bindEvents();
+})
+
+Controller = function(){};
+View = function(){};
+
+
+Controller.prototype = {
+  bindEvents: function(){
+    $("#ipsumButton").on("click", function(e){
+      e.preventDefault();
+      controller.createIpsumAjaxRequest();
+    })
+  },
+
+  createIpsumAjaxRequest: function(){
+    $.ajax({
+      type: "get",
+      url: "/get_ipsum",
+    }).done(function(data){
+      view.renderIpsum(data);
+    })
+  }
+}
+
+View.prototype = {
+  renderIpsum: function(data){
+    $("#ipsumContainer").html(data);
+  }
+}
+
+controller = new Controller();
+view = new View();
